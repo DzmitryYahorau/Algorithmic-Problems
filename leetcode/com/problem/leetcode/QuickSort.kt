2e5c50d5c243f3d746c15c8be3e2fff06ee9a7c1
DzmitryYahorau <dzmitry.yahorau@itechart-group.com>
2020-07-29 01:12:40 +0300

    qs

---
+++ leetcode/com/problem/leetcode/QuickSort.kt
@@ -0,0 +1,30 @@
+class QuickSort {
+
+    private fun sort(array: IntArray, lo: Int, hi: Int) {
+        if (hi <= lo) return
+        var lt = lo
+        var gt = hi
+        val pivot = array[lo]
+        var i = lo
+
+        while (i <= gt) {
+            when {
+                (array[i] < pivot) -> exchange(array, lt++, i++)
+                (array[i] > pivot) -> exchange(array, i, gt--)
+                (array[i] == pivot) -> {
+                    i++
+                }
+            }
+        }
+
+        sort(array, lo, lt - 1)
+        sort(array, gt + 1, hi)
+    }
+
+    private fun exchange(array: IntArray, i: Int, j: Int) {
+        val temp = array[i]
+
+        array[i] = array[j]
+        array[j] = temp
+    }
+}
