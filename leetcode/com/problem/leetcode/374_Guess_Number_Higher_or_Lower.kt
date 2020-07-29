3d3aaaf00b35bedfbccc141dd216112703bf327a
DzmitryYahorau <dzmitry.yahorau@itechart-group.com>
2020-07-29 14:15:05 +0300

    374

---
+++ leetcode/com/problem/leetcode/374_Guess_Number_Higher_or_Lower.kt
@@ -0,0 +1,24 @@
+class GuessNumber() {
+
+    fun guess(a: Int): Int {
+        return when {
+            a < 6 -> 1
+            a > 6 -> -1
+            else -> 0
+        }
+    }
+
+    fun guessNumber(n: Int): Int {
+        return guessRec(1, n)
+    }
+
+    private fun guessRec(lo: Int, hi: Int): Int {
+        val mid = lo + (hi - lo) / 2
+        val guess = guess(mid)
+        when {
+            guess < 0 -> return guessRec(lo, mid)
+            guess > 0 -> return guessRec(mid + 1, hi)
+        }
+        return mid
+    }
+}
