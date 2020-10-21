7af07103eb45094d17bac7129746ec16fa401dbd
DzmitryYahorau <dzmitry.yahorau@itechart-group.com>
2020-10-21 23:31:01 +0300

    48

---
+++ leetcode/com/problem/_problems_test/48_RotateImageTest.kt
@@ -0,0 +1,26 @@
+import array.rotate
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import kotlin.test.assertEquals
+import kotlin.test.assertSame
+
+class `48_RotateImageTest` {
+
+    @Test
+    fun test1() {
+        val actual = arrayOf(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6),
+                intArrayOf(7, 8, 9)
+        )
+
+        rotate(actual)
+        val expected = arrayOf(
+                intArrayOf(7, 4, 1), intArrayOf(8, 5, 2), intArrayOf(9, 6, 3)
+        )
+        assertEquals(
+                actual = actual.joinToString(",") { it.joinToString(",") },
+                expected = expected.joinToString(",") { it.joinToString(",") }
+        )
+    }
+}
