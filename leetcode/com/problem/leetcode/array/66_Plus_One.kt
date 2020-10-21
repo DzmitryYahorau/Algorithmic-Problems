25dc2e581eeac3d94757b64f40743f2f3f30d6ad
DzmitryYahorau <dzmitry.yahorau@itechart-group.com>
2020-10-22 00:57:45 +0300

    66

--- leetcode/com/problem/leetcode/array/66_Plus_One.kt
+++ leetcode/com/problem/leetcode/array/66_Plus_One.kt
@@ -1,23 +1,14 @@
 package array
 
 fun plusOne(digits: IntArray): IntArray {
-
-    var shouldAddOne = true
     for (i in digits.lastIndex downTo 0) {
-        if (!shouldAddOne) break
-
-        shouldAddOne = digits[i] == 9
-
         if (digits[i] == 9) {
             digits[i] = 0
         } else {
-            digits[i] = digits[i] + 1
+            digits[i]++
+            return digits
         }
     }
 
-    return if (shouldAddOne) {
-        intArrayOf(1).plus(digits)
-    } else {
-        digits
-    }
+    return intArrayOf(1).plus(digits)
 }
