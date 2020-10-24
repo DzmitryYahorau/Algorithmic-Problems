ba382a28f10e08ab6251e9ba9564f607b8e9ecbc
DzmitryYahorau <dzmitry.yahorau@itechart-group.com>
2020-10-24 23:24:02 +0300

    111

--- leetcode/com/problem/leetcode/tree/111_Minimum_Depth_of_Binary_Tree.kt
+++ leetcode/com/problem/leetcode/tree/111_Minimum_Depth_of_Binary_Tree.kt
@@ -1,5 +1,8 @@
 package tree
 
+import java.util.*
+
+//DFS
 fun minDepth(root: TreeNode?, sum: Int = 1): Int =
         when {
             (root == null) -> 0
@@ -11,5 +14,33 @@ fun minDepth(root: TreeNode?, sum: Int = 1): Int =
                 minOf(leftPathSum, rightPathSum)
             }
         }
+
+//BFS - should be more efficient
+fun minDepth(root: TreeNode?): Int {
+    return if (root == null) {
+        0
+    } else {
+        val queue: Queue<TreeNode> = LinkedList<TreeNode>()
+        queue.offer(root)
+        var level = 1
+        var i: Int
+        var N: Int
+        var current: TreeNode
+        while (!queue.isEmpty()) {
+            N = queue.size
+            i = 0
+            while (i < N) {
+                current = queue.poll()
+
+                if (current.isLeaf()) return level
+                if (current.left != null) queue.offer(current.left)
+                if (current.right != null) queue.offer(current.right)
+                i++
+            }
+            ++level
+        }
+        -1
+    }
+}
 
 private fun TreeNode.isLeaf(): Boolean = (this.left == null) && (this.right == null)
