2a9166e5845268b60a5591ac1bcc789a28a01d52
DzmitryYahorau <dzmitry.yahorau@itechart-group.com>
2020-11-03 01:43:27 +0300

    9

---
+++ leetcode/com/problem/leetcode/math/9. Palindrome Number.kt
@@ -0,0 +1,23 @@
+package math
+
+fun isPalindrome(x: Int): Boolean {
+    if (x < 0) return false
+    if (x in 0..9) return true
+
+    return x == reversedInt(x)
+}
+
+private fun reversedInt(x: Int): Int {
+    var temp = x
+    var reversedValue = 0
+    while (temp != 0) {
+        reversedValue = (reversedValue * 10) + (temp % 10)
+        temp /= 10
+    }
+
+    return reversedValue
+}
+
+fun main() {
+    isPalindrome(123321)
+}
