e6cd4753c51494cafe1374d9c43e87512d25041e
DzmitryYahorau <dzmitry.yahorau@itechart-group.com>
2021-02-07 23:56:49 +0300

    88

---
+++ leetcode/leetcode/array/88. Merge Sorted Array.kt
@@ -0,0 +1,45 @@
+package array
+
+class Solution88 {
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
+        if (nums1.isEmpty() || nums2.isEmpty()) return
+
+        val size = m + n
+        val aux = IntArray(size = size)
+
+        var i = 0
+        var j = 0
+        var k = 0
+
+        while (k < size) {
+            when {
+                i >= m && j < nums2.size -> {
+                    aux[k++] = nums2[j++]
+                }
+                j >= nums2.size && i < m -> {
+                    aux[k++] = nums1[i++]
+                }
+                nums1[i] <= nums2[j] -> {
+                    aux[k++] = nums1[i++]
+                }
+                nums1[i] > nums2[j] -> {
+                    aux[k++] = nums2[j++]
+                }
+            }
+        }
+
+        for (index in aux.indices) {
+            nums1[index] = aux[index]
+        }
+    }
+}
+
+fun main() {
+
+    val nums1 = intArrayOf(2,0)
+    val m = 1
+    val nums2 = intArrayOf(1)
+    val n = 1
+    Solution88().merge(nums1, m, nums2, n)
+    println(nums1.joinToString(","))
+}
