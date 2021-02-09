6359f2166957063f94f5cd81be0b17c18182e3e7
DzmitryYahorau <dzmitry.yahorau@itechart-group.com>
2021-02-09 20:16:58 +0300

    28

--- leetcode/leetcode/string/28. Implement strStr().kt
+++ leetcode/leetcode/string/28. Implement strStr().kt
@@ -1,21 +1,13 @@
 package string
 
 fun strStr(haystack: String, needle: String): Int {
-    if (haystack.isEmpty() && needle.isEmpty()) return 0
-    if (haystack.isEmpty()) return -1
     if (needle.isEmpty()) return 0
 
-    for (i in haystack.indices) {
-        if (haystack[i] == needle[0] && i + needle.lastIndex <= haystack.lastIndex) {
-
-            val beg = i
-            val end = beg + needle.lastIndex
-
-            if (haystack.slice(beg..end) == needle) {
-                return i
-            } else {
-                continue
-            }
+    for (i in 0..(haystack.lastIndex - needle.lastIndex)) {
+        if (haystack.slice(i..(i + needle.lastIndex)) == needle) {
+            return i
+        } else {
+            continue
         }
     }
 
