d3f84e1ac9eff6c44c57a9d3a06f11da7d97a8ac
DzmitryYahorau <dzmitry.yahorau@itechart-group.com>
2021-02-28 15:07:36 +0300

    821

---
+++ leetcode/leetcode/string/821. Shortest Distance to a Character.kt
@@ -0,0 +1,28 @@
+package string
+
+import kotlin.math.abs
+import kotlin.test.assertEquals
+
+fun shortestToChar(s: String, c: Char): IntArray {
+
+    val result = IntArray(s.length)
+
+    val list = mutableSetOf<Int>()
+
+    s.forEachIndexed { index, char ->
+        if (char == c) {
+            list.add(index)
+        }
+    }
+
+    s.forEachIndexed { index, char ->
+        val closest = list.minBy { abs(it - index) } ?: 0
+        result[index] = abs(closest - index)
+    }
+
+    return result
+}
+
+fun main() {
+    assertEquals(shortestToChar(s = "aaab", c = 'b').joinToString(), intArrayOf(3, 2, 1, 0).joinToString())
+}
