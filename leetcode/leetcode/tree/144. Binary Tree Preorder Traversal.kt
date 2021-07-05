9f8371677408ad002e4b21640039ea00ffcf4694
DzmitryYahorau <dzmitry.yahorau@itechart-group.com>
2021-07-05 21:08:11 +0300

    144 (iterative solution)

--- leetcode/leetcode/tree/144. Binary Tree Preorder Traversal.kt
+++ leetcode/leetcode/tree/144. Binary Tree Preorder Traversal.kt
@@ -1,5 +1,10 @@
 package tree
 
+import java.util.*
+
+/**
+ * Recursive solution
+ */
 fun preorderTraversal(root: TreeNode?): List<Int> {
     val result: MutableList<Int> = mutableListOf()
     preorderTraversal(root, result)
@@ -11,4 +16,23 @@ fun preorderTraversal(root: TreeNode?, result: MutableList<Int>) {
     root?.`val`?.let { result.add(it) }
     root?.left?.let { preorderTraversal(it, result) }
     root?.right?.let { preorderTraversal(it, result) }
+}
+
+/**
+ * Iterative solution
+ */
+fun preorderTraversalIterative(root: TreeNode?): List<Int> {
+    root ?: return emptyList()
+    val result: MutableList<Int> = mutableListOf()
+
+    val stack = Stack<TreeNode>()
+    stack.push(root)
+    while (stack.isNotEmpty()){
+        val node = stack.pop()
+        result.add(node.`val`)
+
+        node.right?.let { stack.push(it) }
+        node.left?.let { stack.push(it) }
+    }
+    return result
 }
